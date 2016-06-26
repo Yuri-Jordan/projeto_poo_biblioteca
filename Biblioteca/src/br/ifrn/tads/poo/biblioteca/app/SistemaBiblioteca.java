1a531fe06fb372c6a8c2c1c1d85b8aca3c70a4ed
Yuri Jordan <yurijrdn@gmail.com>
2016-06-25 21:56:34 -0300

    Pacote view criado.

--- Biblioteca/src/br/ifrn/tads/poo/biblioteca/app/SistemaBiblioteca.java
+++ Biblioteca/src/br/ifrn/tads/poo/biblioteca/app/SistemaBiblioteca.java
@@ -1,7 +1,10 @@
 package br.ifrn.tads.poo.biblioteca.app;
 
+import br.ifrn.tads.poo.view.InterfaceBiblioteca;
+
 public class SistemaBiblioteca {
     public static void main(String[] args) {
-        
+        InterfaceBiblioteca painel = new InterfaceBiblioteca();
+        painel.setVisible(true);
     }
 }
