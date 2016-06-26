148b5440ded2bcf21bdd38c3e24dad04b5d27a6d
ric-luiz <ricardoluiz508@gmail.com>
2016-06-25 21:04:23 -0300

    Adicionado classe Banco. Esta classe cria o banco caso ele não existe e as tabelas caso não exista. Além disso coloca um objeto statment (uma conexão sempre aberta com o banco a partir do inicio da aplicação) disponivel num atributo estatico para qualquer parte do software.

--- Biblioteca/src/br/ifrn/tads/poo/biblioteca/app/SistemaBiblioteca.java
+++ Biblioteca/src/br/ifrn/tads/poo/biblioteca/app/SistemaBiblioteca.java
@@ -1,7 +1,9 @@
 package br.ifrn.tads.poo.biblioteca.app;
 
+import br.ifrn.tads.poo.biblioteca.model.Banco;
+
 public class SistemaBiblioteca {
     public static void main(String[] args) {
-        
+        Banco.conectarBanco();
     }
 }
