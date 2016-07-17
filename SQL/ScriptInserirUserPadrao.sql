f6f3277514535a639d399e0469d1c1e752ddfa07
ric-luiz <ricardoluiz508@gmail.com>
2016-07-17 01:44:23 -0300

    Adicionado os arquivos com o codigo sql das estruturas criadas no banco.

---
+++ SQL/ScriptInserirUserPadrao.sql
@@ -0,0 +1,16 @@
+﻿
+INSERT INTO bibliotecas ( nome )   
+    SELECT 'padrao'
+     WHERE NOT EXISTS (
+       SELECT id
+         FROM bibliotecas 
+        WHERE nome like 'padrao'
+        ) 
+
+INSERT INTO usuarios ( bibliotecas_id,codUsuario,nome,endereco,cpf,role)   
+    SELECT (SELECT id FROM bibliotecas WHERE nome like 'padrao'),0001,'admin','user padrao','00000000000','admin'
+     WHERE NOT EXISTS (
+       SELECT id as uId
+         FROM usuarios 
+        WHERE nome like 'admin'
+        )   
---
+++ SQL/ScriptViewAcervosAll.sql
@@ -0,0 +1,7 @@
+﻿CREATE  or Replace VIEW itemAcervo_all AS
+	SELECT  i.id, i.bibliotecas_id,a.user_id, i.custo, i.codigoitem, i.especifico,
+		a.dataaluguel, a.datadevolucao, a.pago,a.devolvido,a.id as idAlugado
+	FROM itemacervos as i
+	LEFT JOIN alugado as a
+	ON a.id = (SELECT p.id from alugado p where p.itemacervo_id = i.id ORDER BY p.id DESC LIMIT 1)
+	
